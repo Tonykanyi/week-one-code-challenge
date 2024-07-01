e0eacc00fa06bdbb5c909718bcb5959bec25d1f9
Tony <kanyianthony79@gmail.com>
2024-07-02 00:01:02 +0300

    first commit

---
+++ challange2.js
@@ -0,0 +1,20 @@
+
+let speed = parseInt(prompt("Enter the speed of the car: "));
+  
+function calculateDemeritPoints(speed) {
+    if (speed < 70) {
+      console.log("Ok");
+    } else {
+      let demeritPoints = Math.floor((speed - 70) / 5);
+      if (demeritPoints > 12) {
+        console.log("License suspended");
+      } else {
+        console.log(`Points: ${demeritPoints}`);
+      }
+    }
+  }
+  
+
+  
+
+calculateDemeritPoints(speed);
